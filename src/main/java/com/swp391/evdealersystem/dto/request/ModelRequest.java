77152c2304584142d22f8b95530f4d7554f821f8
ngoc son <gowahskoja113@gmail.com>
2025-10-12 00:08:59 +0700

    update(model, jparepositpry):fix logic

--- src/main/java/com/swp391/evdealersystem/dto/request/ModelRequest.java
+++ src/main/java/com/swp391/evdealersystem/dto/request/ModelRequest.java
@@ -6,6 +6,6 @@ import lombok.Setter;
 @Getter
 @Setter
 public class ModelRequest {
-    private String modelCode; // mã model (VD: TESLA-M3, NISSAN-LF)
-    private String name;      // tên model (VD: Model 3, Leaf, Ioniq 6)
+    private String modelCode;
+    private String name;
 }
--- src/main/java/com/swp391/evdealersystem/service/JpaUserDetailsService.java
+++ src/main/java/com/swp391/evdealersystem/service/JpaUserDetailsService.java
@@ -29,7 +29,7 @@ public class JpaUserDetailsService implements UserDetailsService {
         return new org.springframework.security.core.userdetails.User(
                 user.getEmail(),
                 user.getPassword(),
-                List.of(authority)  // chỉ có 1 role
+                List.of(authority)
         );
     }
 
