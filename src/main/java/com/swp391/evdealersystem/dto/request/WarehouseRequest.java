214fda6d1b30a3e0c3c5f48487e0cefc5fe3ea9c
ngoc son <gowahskoja113@gmail.com>
2025-10-23 14:22:27 +0700

    update(warehouse): update warehouse name

--- src/main/java/com/swp391/evdealersystem/dto/request/WarehouseRequest.java
+++ src/main/java/com/swp391/evdealersystem/dto/request/WarehouseRequest.java
@@ -10,4 +10,9 @@ public class WarehouseRequest {
     @NotBlank(message = "Warehouse location is required")
     @Size(max = 255, message = "Location cannot exceed 255 characters")
     private String warehouseLocation;
+
+    @NotBlank(message = "Warehouse name is required")
+    @Size(max = 100, message = "Warehouse name cannot exceed 100 characters")
+    private String warehouseName;
+
 }
